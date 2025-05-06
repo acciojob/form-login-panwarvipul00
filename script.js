14f34d798f59094abf9d2405f39557e6f974879a
panwarvipul00 <panwarvipul1234@gmail.com>
2025-05-06 14:42:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getFormvalue(event) {
 	event.preventDefault();
 	const firstname=document.querySelector('input[name="fname"]').value;
 	const lastname=document.querySelector('input[name="lname"]').value;
-	const message=`First Name: ${firstName} Last Name: ${lastName}`;
+	const message = `First Name: ${firstname} Last Name: ${lastname}`;
 	alert(message);
     //Write your code here
 
