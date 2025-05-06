3dffcd2985ca9a924d5fafc6a573ddc16b33e53d
panwarvipul00 <panwarvipul1234@gmail.com>
2025-05-06 14:38:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
+function getFormvalue(event) {
+	event.preventDefault();
+	const firstname=document.querySelector('input[name="fname"]').value;
+	const lastname=document.querySelector('input[name="lname"]').value;
+	const message=`First Name: ${firstName} Last Name: ${lastName}`;
+	alert(message);
     //Write your code here
 
 }
